c205f36ba3f9e5da4a63a63dd5de277bfee55ba3
nyabingenorv <norvyphill@gmail.com>
2023-10-13 12:29:11 +0300

    console: -main_loop -rev 2

--- console.py
+++ console.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
 '''Running point for CMD loop'''
-message = '(hbnb)'
-controller
-while 
-print()
+# message to display
+cmd = '(hbnb)'
+
+message = '''Documented commands (type help <topic>):
+========================================
+EOF  help  quit'''
+# main loop controller
+controller = 1
+# buffer for user input
+user_input = ''
+while controller:
+    # Taking user input
+    user_input = input(cmd)
+    # Help and closing Decision
+    if user_input == 'help':
+
