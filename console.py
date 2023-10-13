6311236b84611ba2d56460e404a28228ba575e59
nyabingenorv <norvyphill@gmail.com>
2023-10-13 13:07:14 +0300

    console: -interactive -rev 4

--- console.py
+++ console.py
@@ -1,20 +1,28 @@
 #!/usr/bin/python3
 '''Running point for CMD loop'''
-# message to display
-cmd = '(hbnb)'
 
-message = '''Documented commands (type help <topic>):
-========================================
-EOF  help  quit'''
-# main loop controller
-controller = 1
-# buffer for user input
-user_input = ''
-while controller:
-    # Taking user input
-    user_input = input(cmd)
-    # Help and closing Decision
-    if user_input == 'help':
-        print(message)
-    elif user_input == 'quit':
-        controller = 0
+
+def main_runner():
+    '''console runner'''
+    # message to display
+    cmd = '(hbnb)'
+
+    message = '''Documented commands (type help <topic>):
+    ========================================
+    EOF  help  quit'''
+    # main loop controller
+    controller = 1
+    # buffer for user input
+    user_input = ''
+    while controller:
+        # Taking user input
+        user_input = input(cmd).lower()
+        # Help and closing Decision
+        if user_input == 'help':
+            print(message)
+        elif user_input == 'quit':
+            controller = 0
+
+
+if __name__ == "__main__":
+    main_runner()
