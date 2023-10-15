49ce8574c5856656ad38b5759cc5547468f23a94
nyabingenorv <norvyphill@gmail.com>
2023-10-15 13:37:57 +0300

    models: -interactive -rev 3

--- models/city.py
+++ models/city.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""The city class"""
+
+
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """
+    Inherit BaseModel
+    """
+    state_id = ""
+    name = ""
